0d64cd78c0754f5f9e5e35d18852c817c4172cb5
Stephanowicz <elijah@gmx.de>
2024-11-16 12:59:44 +0100

    Update commands.php

--- addons/Stephanowicz/commands.php
+++ addons/Stephanowicz/commands.php
@@ -1,7 +1,8 @@
 <?php
 /*
  * SPDX-License-Identifier: GPL-3.0-or-later
- * Copyright 2014 The moOde audio player project / Tim Curtis
+ * Copyright 2024 Stephanowicz 
+ * https://github.com/Stephanowicz/moOde-audioplayer-addons
 */
 
 require_once __DIR__ . '/../../inc/common.php';
