9a00b1d7e1d4158cd91bdd91fd116ce7e7a1e2d9
Stephanowicz <elijah@gmx.de>
2024-11-16 12:56:51 +0100

    Update geniuslyrics.php

--- addons/Stephanowicz/lyrics/geniuslyrics.php
+++ addons/Stephanowicz/lyrics/geniuslyrics.php
@@ -1,27 +1,9 @@
 <?php
-/*!
- * moOde audio player (C) 2014 Tim Curtis
- * http://moodeaudio.org
- *
- * (C) 2020 Stephanowicz
- * https://github.com/Stephanowicz
- *
- * This Program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
- *
- * This Program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * 2020-12-15 TC moOde 7.0.0
- *
- */
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright 2024 Stephanowicz 
+ * https://github.com/Stephanowicz/moOde-audioplayer-addons
+*/
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
 $ClientAccessToken = "";
