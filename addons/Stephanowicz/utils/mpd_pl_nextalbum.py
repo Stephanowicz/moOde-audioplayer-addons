d3300f3796c72170f4bb8c1c2aa03e9facd9bf92
Stephanowicz <elijah@gmx.de>
2025-05-30 11:02:57 +0200

    Add files via upload
    utility scripts
    Added folder for little helpers
    -- mpd_pl_nextalbum.py :  
      In case You have an playlist with different albums (at least different folders) You can skip forward/backward to next/previous album / folder with this  
      e.g You may add it to remote control with lirc :   
      begin  
       prog = irexec  
       button = KEY_RIGHT  
       config = python /var/www/addons/Stephanowicz/utils/mpd_pl_nextalbum.py  
     end
    
    without argument it skips forward with 'back' as argument it skips ....else :)

--- addons/Stephanowicz/utils/mpd_pl_nextalbum.py
+++ addons/Stephanowicz/utils/mpd_pl_nextalbum.py
@@ -17,11 +17,17 @@
 
 import sys
 import os
+
+direction = 1
+if "back" in sys.argv:
+	direction = -1
+
 stream = os.popen('mpc -f "%file%" playlist')
 mpclist = stream.read().splitlines()
 mpclen=len(mpclist)
 stream = os.popen('mpc status %songpos%')
 mpcindex = stream.read()
+index = 0
 if mpclen > 0 and int(mpcindex) < (mpclen - 2):
 	mpcsong = mpclist[int(mpcindex)-1]
 #	print("mpcsong: " + mpcsong)
@@ -30,17 +36,35 @@ if mpclen > 0 and int(mpcindex) < (mpclen - 2):
 		mpcalbum = mpcsong[0:mpcalbumindex]
 #		print("mpcalbum: " + mpcalbum)
 		index = int(mpcindex)
-#		for index in range(int(mpcindex),mpclen-1):
-		while index < mpclen-1:
-			song = mpclist[index]
-			songalbumindex = song.rfind('/')
-			if songalbumindex > 0:
-				songalbum  = song[0:songalbumindex]
-				if songalbum != mpcalbum: 
-					print("songalbum: " + songalbum + " index: " + str(index+1))
-				#	mpcalbum = songalbum
-					break
-			index +=1
-		if index == mpclen-1:
-			index = 0
-		os.popen('mpc play ' + str(index+1))
+#		for i in range(int(mpcindex),mpclen-1):
+		if direction > 0:
+			while index < mpclen-1:
+				song = mpclist[index]
+				songalbumindex = song.rfind('/')
+				if songalbumindex > 0:
+					songalbum  = song[0:songalbumindex]
+					if songalbum != mpcalbum: 
+						print("songalbum: " + songalbum + " index: " + str(index+1))
+					#	mpcalbum = songalbum
+						break
+				index += 1
+		else:
+			found = False
+			while index > 0:
+				song = mpclist[index]
+				songalbumindex = song.rfind('/')
+				if songalbumindex > 0:
+					songalbum  = song[0:songalbumindex]
+					if songalbum != mpcalbum: 
+						print("songalbum: " + songalbum + " index: " + str(index+1))
+						if found:
+							index += 1
+							break
+						found = True
+						mpcalbum = songalbum						
+				index -= 1
+		
+		
+if index >= mpclen-1 or index < 0:
+	index = 0
+os.popen('mpc play ' + str(index+1))
