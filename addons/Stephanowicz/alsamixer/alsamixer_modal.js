e661226628c6f56a28a642987b1d51fe0ad0bf92
Stephanowicz <elijah@gmx.de>
2025-08-15 11:46:54 +0200

    v2.0 - new structure
    - outsourcing of functions in seperate files  
    - minor fixes and optimisations
      
    if You are using a previous version, please make sure You remove old files, update the changed files and add new files  
    or just delete the whole folder and add the new one (if You have a genius clientaccesstoken - save the lyrics/clientaccesstoken.txt before!)

---
+++ addons/Stephanowicz/alsamixer/alsamixer_modal.js
@@ -0,0 +1,21 @@
+var tempstr = '<div id="ALSAGraphicsEQ-modal" class="hide" tabindex="-1" role="dialog" aria-labelledby="ALSAGraphicsEQ-modal-label" aria-hidden="true" style="transform: translate(-50%);' +
+	'border-radius: 6px;' +
+	'left: 50%;' +
+	'position: fixed;' +
+	'z-index: 10001;' +
+	'box-shadow: 0 3px 7px rgba(0,0,0,.3);"></div>';
+$("#shutdown").after(tempstr);
+
+tempstr = '<li><a href="#notarget" data-cmd="" data-addoncmd="ALSAgraphicEQ"><i class="fal fa-sliders fa-rotate-90 sx"></i>Graphic EQ</a></li>';
+$("#context-menu-playback ul").append(tempstr);
+
+//****************************ALSAgraphicEQ()***********************************************
+function ALSAgraphicEQ() {
+    //$('#ALSAGraphicsEQ').empty();	
+	$('#ALSAGraphicsEQ-modal').empty();
+	$('#ALSAGraphicsEQ-modal').load('addons/Stephanowicz/alsamixer/alsamodal.txt', function() {
+		$('#ALSAGraphicsEQ').load('addons/Stephanowicz/alsamixer/ALSAGraphicsEQ.html');
+		$('#ALSAGraphicsEQ-modal').modal();
+	});		
+}
+//******************************************************************************************
