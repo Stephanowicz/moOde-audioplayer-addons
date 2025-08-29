1f61a6953825c2616a6a2173b063f116832b1552
Stephanowicz <elijah@gmx.de>
2025-08-29 20:35:57 +0200

    Add files via upload

--- addons/Stephanowicz/js/playqueue.js
+++ addons/Stephanowicz/js/playqueue.js
@@ -2,6 +2,7 @@
 var tempstr = '<div id="plstat" style="position: absolute; bottom: 0px; margin-left: 1.2em;"></div>';
 $("#container-playqueue").after(tempstr);
 
+plStatDisp();
 //extend function updKnobStartFrom() & renderReconnect() in playerlib.js for updating remaining playlisttime --> plStatDisp()
 var updKnobStartFrom_extended = updKnobStartFrom;
 var renderReconnect_extended = renderReconnect;
@@ -25,26 +26,26 @@ var plStat = [];
 function plStatDisp() {
     $.getJSON('addons/Stephanowicz/commands.php?cmd=plStatus', function (data) {
         plStat = data;
-    });
 
-    if (plStat['pltotal']) {
-        if (plStat['state'] === 'stop') {
-            $('#plstat').html("[" + plStat['playlistlength'] + "] [" + plStat['pltotal'] + "]");
-        }
-        else {
-            if (SESSION.json['timecountup'] == "1" || parseInt(plStat['time']) == 0) {
-                var elapsed = plStat['plelapsed'].split(':').reduce((acc, time) => (60 * acc) + +time) + $('#countdown-display')[0].textContent.split(':').reduce((acc, time) => (60 * acc) + +time);
-                var elapsed_formatted = new Date(elapsed * 1000).toISOString().slice(11, 19);
-                $('#plstat').html("[" + (parseInt(plStat['song']) + 1) + " / " + plStat['playlistlength'] + "] [" + elapsed_formatted + " / " + plStat['pltotal'] + "]");
-            }
-            else {
-                var remaining = plStat['plremaining'].split(':').reduce((acc, time) => (60 * acc) + +time);
-                remaining -= plStat['time'] != null ? parseInt(plStat['time']) - $('#countdown-display')[0].textContent.split(':').reduce((acc, time) => (60 * acc) + +time) : 0;
-                var remaining_formatted = new Date(remaining * 1000).toISOString().slice(11, 19);
-                $('#plstat').html("[" + (parseInt(plStat['song']) + 1) + " / " + plStat['playlistlength'] + "] [" + remaining_formatted + " / " + plStat['pltotal'] + "]");
-            }
-        }
-    }
+		if (plStat['pltotal']) {
+			if (plStat['state'] === 'stop') {
+				$('#plstat').html("[" + plStat['playlistlength'] + "] [" + plStat['pltotal'] + "]");
+			}
+			else {
+				if (SESSION.json['timecountup'] == "1" || parseInt(plStat['time']) == 0) {
+					var elapsed = plStat['plelapsed'].split(':').reduce((acc, time) => (60 * acc) + +time) + $('#countdown-display')[0].textContent.split(':').reduce((acc, time) => (60 * acc) + +time);
+					var elapsed_formatted = new Date(elapsed * 1000).toISOString().slice(11, 19);
+					$('#plstat').html("[" + (parseInt(plStat['song']) + 1) + " / " + plStat['playlistlength'] + "] [" + elapsed_formatted + " / " + plStat['pltotal'] + "]");
+				}
+				else {
+					var remaining = plStat['plremaining'].split(':').reduce((acc, time) => (60 * acc) + +time);
+					remaining -= plStat['time'] != null ? parseInt(plStat['time']) - $('#countdown-display')[0].textContent.split(':').reduce((acc, time) => (60 * acc) + +time) : 0;
+					var remaining_formatted = new Date(remaining * 1000).toISOString().slice(11, 19);
+					$('#plstat').html("[" + (parseInt(plStat['song']) + 1) + " / " + plStat['playlistlength'] + "] [" + remaining_formatted + " / " + plStat['pltotal'] + "]");
+				}
+			}
+		}
+    });
 }
 
 function render_plstat() {
